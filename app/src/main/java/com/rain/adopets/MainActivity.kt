e01da9c69c2d6d43880acf5cfb827d2c5defb631
chrix0 <christiantopratama@gmail.com>
2021-12-03 12:09:35 +0700

    Initial commit

---
+++ app/src/main/java/com/rain/adopets/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.rain.adopets
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.navigation.NavigationBarMenuView
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val navController = findNavController(R.id.fragmentContainerView)
+        navBottom.setupWithNavController(navController)
+    }
+}
