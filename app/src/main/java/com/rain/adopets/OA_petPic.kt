d74e0c4d56f16b3d97b804bb7e6982a9548c403f
chrix0 <christiantopratama@gmail.com>
2021-12-08 19:38:15 +0700

    design product list + analyzer step 1

---
+++ app/src/main/java/com/rain/adopets/OA_petPic.kt
@@ -0,0 +1,11 @@
+package com.rain.adopets
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OA_petPic : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_oa_pet_pic)
+    }
+}
