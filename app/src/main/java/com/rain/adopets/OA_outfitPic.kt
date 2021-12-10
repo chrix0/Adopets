29857c45be18fdb294af4d01901018941c5f078d
chrix0 <christiantopratama@gmail.com>
2021-12-10 18:46:05 +0700

    design OA (all)

---
+++ app/src/main/java/com/rain/adopets/OA_outfitPic.kt
@@ -0,0 +1,11 @@
+package com.rain.adopets
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OA_outfitPic : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_oa_outfit_pic)
+    }
+}
---
+++ app/src/main/java/com/rain/adopets/OA_result.kt
@@ -0,0 +1,11 @@
+package com.rain.adopets
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OA_result : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_oa_result)
+    }
+}
