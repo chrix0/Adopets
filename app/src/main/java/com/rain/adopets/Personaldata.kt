15846047310209e04b7e052b67de7d0e63327b92
praaannnn <fransxianiparz@gmail.com>
2022-01-05 15:37:55 +0700

    menu login

--- app/src/main/java/com/rain/adopets/Personaldata.kt
+++ app/src/main/java/com/rain/adopets/Personaldata.kt
@@ -14,13 +14,16 @@ class Personaldata : AppCompatActivity() {
 
         name.setText(user.fullName)
         email.setText(user.email)
-        address.setText(singletonData.formatAlamat(user.shippingAddress))
+        address.setText(user.shippingAddress[0])
+        City.setText(user.shippingAddress[1])
+        Province.setText(user.shippingAddress[2])
+        psCode.setText(user.shippingAddress[3])
         Pnumber.setText(user.phoneNumber)
 
         save.setOnClickListener{
             singletonData.accList[singletonData.currentAccId].fullName = name.text.toString()
             singletonData.accList[singletonData.currentAccId].email = email.text.toString()
-            user.shippingAddress = mutableListOf<String>(address.text.toString(), City.text.toString(), Province.text.toString(), psCode.text.toString())
+            user.shippingAddress = mutableListOf<String>(address.text.toString(), City.text.toString(),Province.text.toString(),psCode.text.toString())
             singletonData.accList[singletonData.currentAccId].phoneNumber = Pnumber.text.toString()
             var intent = Intent(this,MainActivity::class.java)
             intent.putExtra(RETURN_LAST_TAB, "PROFILE")
