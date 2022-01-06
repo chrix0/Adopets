e63031f55ee18c5af3ba9a494eeb36e5672db69a
chrix0 <christiantopratama@gmail.com>
2022-01-06 10:51:12 +0700

    Tambahan di AndroidManifest: Icon app, splash screen, anti-rotate

--- app/src/main/java/com/rain/adopets/splash_screen.kt
+++ app/src/main/java/com/rain/adopets/splash_screen.kt
@@ -3,14 +3,18 @@ package com.rain.adopets
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
+@Suppress("Deprecation")
 class splash_screen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
         //PLAN : GIVE MAIN + LAUNCHER
-        var intent = Intent(this, MainActivity::class.java)
-        Thread.sleep(3000)
-        startActivity(intent)
+        Handler().postDelayed(Runnable{
+            var intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
     }
 }
